2cc5f35c8bc7d971356599eb8039c27878dbdd86
Víctor Terraga <victor.terraga@entrust.com>
2024-07-15 20:42:51 +0200

    colemak default

--- keymap.c
+++ keymap.c
@@ -19,8 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum layers
 {
-    _ALPHA_QWERTY = 0,
-    _ALPHA_COLEMAK,
+    _ALPHA_COLEMAK = 0,
+    _ALPHA_QWERTY,
     _SYM,
     _NAV,
     _NUM,
@@ -29,19 +29,18 @@ enum layers
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    
+    [_ALPHA_COLEMAK] = LAYOUT(
+        KC_Q,         KC_W,    KC_F,    KC_P,    KC_G,                                                   KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
+        KC_A,         KC_R,    KC_S,    KC_T,    KC_D,                                                   KC_H,    KC_N,    KC_E,    KC_I,    KC_O,
+        LSFT_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                                                   KC_K,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SCLN),
+                        LCTL_T(KC_ENT), LT(_NUM,KC_SPC), LGUI_T(KC_TAB),     LT(_SYM, KC_BSPC), LT(_NAV,KC_ENT), LALT_T(KC_DEL)         
+    ),    
 [_ALPHA_QWERTY] = LAYOUT(
         KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                                                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  
         KC_A,         KC_S,    KC_D,    KC_F,    KC_G,                                                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         LSFT_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                                                   KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
             
                         LCTL_T(KC_ESC), LT(_NUM,KC_SPC), LT(_NAV, KC_TAB),     LT(_SYM, KC_BSPC), LGUI_T(KC_ENT), LALT_T(KC_DEL)         
-    ),
-    [_ALPHA_COLEMAK] = LAYOUT(
-        KC_Q,         KC_W,    KC_F,    KC_P,    KC_G,                                                   KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
-        KC_A,         KC_R,    KC_S,    KC_T,    KC_D,                                                   KC_H,    KC_N,    KC_E,    KC_I,    KC_O,
-        LSFT_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                                                   KC_K,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SCLN),
-                        LCTL_T(KC_ENT), LT(_NUM,KC_SPC), LT(_NAV, KC_TAB),     LT(_SYM, KC_BSPC), KC_ENT, LALT_T(KC_DEL)         
     ),
     [_SYM] = LAYOUT(
         KC_GRV , KC_CIRC,   KC_AT,  KC_DLR, KC_TILD,                                KC_AMPR, KC_EXLM, KC_PIPE, KC_UNDS, KC_HASH,
